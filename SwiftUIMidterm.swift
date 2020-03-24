f8cd52e362c51655faccdb853d7eadc391407e37
Pyerce <propyerce@gmail.com>
2020-03-23 22:01:46 -0700

    Add files via upload
    Added Files from Midterm for 411 to portfolio

---
+++ SwiftUIMidterm.swift
@@ -0,0 +1,52 @@
+
+
+//Number 1: In SwiftUI, write the code that would show the view to the right when running.
+
+struct ContentView: View {
+     var body: some View {
+        Text("I'm an iOS Developer!").padding().frame(width:100, height: 100)
+            .background(Color(red: 0, green: 2, blue: 1))
+    }
+}
+
+// Number 2: In SwiftUI, write the code that would show the view to the right when running (“Live long and prosper” is written in the center)
+struct ContentView: View {
+     var body: some View {
+        Text("Live long and prosper").padding().frame(width:100, height: 100)
+            .background(Color.red)
+    }
+}
+
+
+
+
+// Number 3: In SwiftUI, write the code to show a form with “Hello, world” shown twice, and with with a Navigation bar with the text “SwiftUI” shown in its title.
+struct ContentView: View {
+    var body: some View{
+           NavigationBar {
+             Form {
+    Section {
+        Text("Hello, world!");
+        Text("Hello, world!")
+                }
+                Section { Text("Hello, world!") }
+    }.navigationBarTitle(“SwiftUI”)
+        }
+    }
+}
+
+
+// Number 4 and 5 Write the SwiftUI code to have a button that shows its tapCount when tapped on.
+struct ContentView: View {
+      var body: some View{
+        Button(action: { self.tapCount += 1}) {
+            Text("Tap count: \(self.tapCount)")
+                .padding()
+                .frame(width: 100, height: 100)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+            }
+        }
+    }
+}
